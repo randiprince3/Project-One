0e9735ea0b50692d20e99b5e301cb999156331b8
Randi Prince <randiprince3@MacBook-Pro-7.local>
2018-07-17 18:30:54 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,44 @@
-
-// debugger;
-var ticketQueryURL="https://app.ticketmaster.com/discovery/v2/events.json?apikey=j3c9q4m90n44zYYlgilg9IL5rjkdY3Ux&keyword=";
-var nytQueryUrl=0;
-var table=$("<table>");
-
 $(".button").on("click",function(){
-    console.log(userSearch);
-    var userSearch = $("#artist-search").val();
+    event.preventDefault();
+    var ticketQueryURL="https://app.ticketmaster.com/discovery/v2/events.json?apikey=j3c9q4m90n44zYYlgilg9IL5rjkdY3Ux&keyword=";
+    var userSearch = $("#artist-search").val().trim();
+
     $ .ajax({
-        url:ticketQueryURL + userSearch,
+        url: ticketQueryURL + userSearch,
         method:"GET",
     }).then(function(response){
         console.log(response);
         
-        for(var i=0;i<3;i++){
-           var showName=response._embedded.events[i].name;
-           var dates=response._embedded.events[i].dates.start.localDate;
-           var venue=response._embedded.events[i]._embedded.venues[0].name;
-           var cityName=response._embedded.events[i]._embedded.venues[0].city.name;
-           var newRow = $("<tr>").append(
-            $("<td>").text(showName),
-            $("<td>").text(dates),
-            $("<td>").text(venue),
-            $("<td>").text(cityName)
-          );
+        $(".responsive-table > tbody").empty();
 
-           $(".responsive-table > tbody").append(newRow);
-        }
+        for(var i=0; i < 3; i++){
+           var showName = response._embedded.events[i].name;
+
+           var dates = response._embedded.events[i].dates.start.localDate;
+           var randomFormat = "YYYY/MM/DD";
+           var convertedDate = moment(dates, randomFormat);
+           var artistDate = moment(convertedDate).format("MM/DD/YY");
+
+           var venue = response._embedded.events[i]._embedded.venues[0].name;
+           var cityName = response._embedded.events[i]._embedded.venues[0].city.name;
+           var eventLink = showName.link(response._embedded.events[i].url);
         
+           var newRow = $("<tr>").append(
+            $("<td>").html(eventLink),
+            $("<td>").text(venue),
+            $("<td>").text(artistDate),
+            $("<td>").text(cityName),
+            );
+
+          $(".responsive-table > tbody").append(newRow);
+        }
          
     });
 
-});
-
-// Randi Code
-$(".button").on("click", function() {
-
-    event.preventDefault();
-
-    var artistInput = $("#artist-search").val().trim();
-
-    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bc8827527e974e0dba93f18bb17c76a3&q=" + artistInput;
+    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bc8827527e974e0dba93f18bb17c76a3&q=" + userSearch;
     console.log(queryURL);
+
+    $(".card-content").html("<h5>Current Events for " + userSearch.substring(0,1).toUpperCase() + userSearch.substring(1).toLowerCase() + "</h5>");
 
     $.ajax({
         url: queryURL,
@@ -49,21 +46,29 @@ $(".button").on("click", function() {
     }).then(function(response) {
         console.log(response);
 
+        $("#article-section").empty();
+
         for (var i = 0; i < 3; i++) {
-            var articleDiv = $("<p>")
-            var article = response.response.docs[i].snippet
+            var articleDiv = $("<p>");
+            var headline = response.response.docs[i].headline.main;
             
-            articleDiv.text(article);
+            articleDiv.text(headline);
+
             $("#article-section").append(articleDiv);
 
-            console.log(article);
+            console.log(headline);
 
-            var nytURL = response.response.docs[i].web_url
-                console.log(nytURL);
-            var showURL = $("<p>").text("Link: " + nytURL);
+            var nytURL = response.response.docs[i].web_url;
+            console.log(nytURL);
+            var snippet = response.response.docs[i].snippet;
+            var openLink = snippet.link(nytURL);
+                
+            var showURL = $("<p>").html("See the full article here: " + openLink);
+            
             $(articleDiv).append(showURL);
-        }
-        
+
+        } 
 
     });
+
 });
