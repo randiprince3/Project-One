33c689fc2d59851f6e2bb3e79639236d90106b75
alexg44 <alex.gshon@gmail.com>
2018-07-19 21:04:02 -0700

    final?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,37 @@
-$(".button").on("click",function(){
+var config = {
+    apiKey: "AIzaSyBLQpKlUuikhGH1iWmpgIMfAFA6G8go0ro",
+    authDomain: "project-one-bc508.firebaseapp.com",
+    databaseURL: "https://project-one-bc508.firebaseio.com",
+    projectId: "project-one-bc508",
+    storageBucket: "project-one-bc508.appspot.com",
+    messagingSenderId: "471673883230"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+var search = "";
+
+
+$("#run-search").on("click",function(){
+
     event.preventDefault();
-    var ticketQueryURL="https://app.ticketmaster.com/discovery/v2/events.json?apikey=j3c9q4m90n44zYYlgilg9IL5rjkdY3Ux&keyword=";
     var userSearch = $("#artist-search").val().trim();
+    var ticketQueryURL="https://app.ticketmaster.com/discovery/v2/events.json?apikey=j3c9q4m90n44zYYlgilg9IL5rjkdY3Ux&keyword=";
+    
+ if (userSearch === "") {
+    M.toast({html: 'You must enter something in the search bar', classes: "blue 1000"})
+} else {
+    database.ref().push({
+        search: userSearch,
+      });
 
     $ .ajax({
         url: ticketQueryURL + userSearch,
         method:"GET",
     }).then(function(response){
         console.log(response);
-        var $tbody = $("table#artist-events > tbody");
-        $tbody.empty();
+        
+        $(".striped > tbody").empty();
 
         for(var i=0; i < 3; i++){
            var showName = response._embedded.events[i].name;
@@ -21,16 +43,16 @@ $(".button").on("click",function(){
 
            var venue = response._embedded.events[i]._embedded.venues[0].name;
            var cityName = response._embedded.events[i]._embedded.venues[0].city.name;
-           var eventLink = showName.link(response._embedded.events[i].url);
+           var eventLink = $('<a>').attr('href', response._embedded.events[i].url).attr('target', '_blank').append(showName);
         
            var newRow = $("<tr>").append(
-                $("<td>").html(eventLink),
-                $("<td>").text(venue),
-                $("<td>").text(artistDate),
-                $("<td>").text(cityName),
+            $("<td>").html(eventLink),
+            $("<td>").text(venue),
+            $("<td>").text(artistDate),
+            $("<td>").text(cityName),
             );
 
-          $tbody.append(newRow);
+          $(".striped > tbody").append(newRow);
         }
          
     });
@@ -38,8 +60,8 @@ $(".button").on("click",function(){
     var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bc8827527e974e0dba93f18bb17c76a3&q=" + userSearch;
     console.log(queryURL);
 
-    $(".card-content").html("<h5>Current Events for " + userSearch.substring(0,1).toUpperCase() + userSearch.substring(1).toLowerCase() + "</h5>");
-
+    $(".card-content").html("<h5>Current Events for " + userSearch.substring(0,1).toUpperCase() + userSearch.substring(1).toLowerCase() + "</h5>").addClass("new-info");
+    
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -49,26 +71,26 @@ $(".button").on("click",function(){
         $("#article-section").empty();
 
         for (var i = 0; i < 3; i++) {
-            var articleDiv = $("<p>").addClass("newsArticles");
+            var articleDiv = $("<p>");
             var headline = response.response.docs[i].headline.main;
             
-            articleDiv.text(headline);
+            articleDiv.html("<u><strong>" + headline + "</strong></u>").addClass("headline-style");
 
             $("#article-section").append(articleDiv);
 
             console.log(headline);
 
-            var nytURL = response.response.docs[i].web_url;
-            console.log(nytURL);
             var snippet = response.response.docs[i].snippet;
-            var openLink = snippet.link(nytURL);
+            var openLink = $('<a>').attr('href', response.response.docs[i].web_url).attr('target', '_blank').append(snippet);
                 
-            var showURL = $("<p>").html("See the full article here: " + openLink);
+            var showURL = $("<p>").append("Link: ", openLink);
             
             $(articleDiv).append(showURL);
 
-        } 
-
+        }
+    
     });
-
-});
+}
+    $("#artist-search").val("");
+    
+    });
